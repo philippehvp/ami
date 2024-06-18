67fca311e36387fed7cb92203ec8586b2b0df096
philippehvp <philippe_hvp@hotmail.com>
2024-06-18 14:12:14 +0200

    Nouveaux ajustements graphiques

--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -49,7 +49,6 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   private destroy$!: Subject<boolean>;
   private _isRelog!: boolean;
-  private _splashClass: string = 'splash';
 
   public get disabledLoginButton(): boolean {
     const name: string = this.formGroup?.get(['name'])?.value || '';
@@ -61,10 +60,6 @@ export class LoginComponent implements OnInit, OnDestroy {
     if (this._isRelog) return this._isRelog;
 
     return false;
-  }
-
-  public get splashClass(): string {
-    return this._splashClass;
   }
 
   public ngOnInit() {
@@ -154,10 +149,6 @@ export class LoginComponent implements OnInit, OnDestroy {
         }
       });
     }
-  }
-
-  public enter() {
-    this._splashClass += ' fadeOut';
   }
 
   public createBetter() {
